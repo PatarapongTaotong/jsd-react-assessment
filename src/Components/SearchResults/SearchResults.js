d83026c979dd23570cce8c488a54cb39f95e5fef
Patarapong Taotong <t.patarapong@gmail.com>
2022-03-17 17:34:46 +0700

    Create Static Components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import './SearchResults.css';
+
+const SearchResults = () => {
+    return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+            {/* <TrackList /> */}
+        </div>
+    );
+}
+
+export default SearchResults;
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,23 @@
+import './Track.css';
+
+const Track = () => {
+    const renderAction = (props) => {
+        if (props.isRemoval) {
+            return <button className="Track-action">+-</button>;
+        } else {
+            return <button className="Track-action">+</button>;
+        }
+    }
+
+    return (
+        <div className="Track">
+            <div className="Track-information">
+                <h3>track name</h3>
+                <p>track artist | track album</p>
+            </div>
+            {renderAction()}
+        </div>
+    );
+}
+
+export default Track;
